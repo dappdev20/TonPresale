4ef529f5ca46a0815b9904b3cdc9af2b5cb6a8d9
dappdev20 <olehden20@gmail.com>
2024-09-30 07:57:08 +0000

    Update social links.

--- app/page.tsx
+++ app/page.tsx
@@ -6,30 +6,50 @@ export default function Home() {
   return (
     <div className="background">
       <div className="w-screen h-screen flex justify-center ">
-      <main className="flex flex-col w-[40%] items-center text-white justify-center">
-        <div className="flex gap-4">
-          <img src="/images/logo.svg"></img>
-          <label className="titleFont text-[27px]">Elluminex</label>
-        </div>
-
-        <div className="flex flex-col mt-6" style={{ lineHeight: 'normal'}}>
-          <div className="titleFont text-[56px] text-center"><b>The Future of Defi</b> on</div>
-          <div className="titleFont text-[56px] text-center">the TON blockchain</div>
-        </div>
-
-        <div className="flex flex-col mt-2">
-          <div>
-            <label className="titleFont text-[16px] text-center">Presale Launch Coming Soon!</label>
+        <main className="flex flex-col w-[40%] items-center text-white justify-center">
+          <div className="flex gap-4">
+            <img src="/images/logo.svg"></img>
+            <label className="titleFont text-[27px]">Elluminex</label>
           </div>
 
-          <div className="flex justify-center gap-4 mt-8 w-full">
-            <img src="/images/twitter.png"></img>
-            <img src="/images/medium.png"></img>
-            <img src="/images/telegram.png"></img>
-            <img src="/images/youtube.png"></img>
+          <div className="flex flex-col mt-6" style={{ lineHeight: 'normal' }}>
+            <div className="titleFont text-[56px] text-center"><b>The Future of Defi</b> on</div>
+            <div className="titleFont text-[56px] text-center">the TON blockchain</div>
           </div>
-        </div>
-      </main>
+
+          <div className="flex flex-col mt-2">
+            <div>
+              <label className="titleFont text-[16px] text-center">Presale Launch Coming Soon!</label>
+            </div>
+
+            <div className="flex justify-center gap-4 mt-8 w-full">
+              <a href="https://x.com/elluminex"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="/images/twitter.png"></img>
+              </a>
+              <a href="https://medium.com/@elluminex"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="/images/medium.png"></img>
+              </a>
+              <a href="https://t.me/Elluminex"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="/images/telegram.png"></img>
+              </a>
+              <a href="https://www.youtube.com/@Elluminex"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img src="/images/youtube.png"></img>
+              </a>
+            </div>
+          </div>
+        </main>
       </div>
       {/* <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
           <a
